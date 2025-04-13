61082fdf619a499f65ef45e0708014ffc6ec3dac
GG2shadow <xyj.mine@gmail.com>
2025-04-13 19:31:35 +0800

    Fixed .mdx readability by Vercel.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+// next.config.js
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+}
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig)
