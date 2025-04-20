1c218f9d55c7f75aad6301e5f1bc72bd16c9bc64
daudjulio54@gmail.com <daudjulio54@gmail.com>
2025-04-20 23:15:14 +0700

    Remove all console.log when building.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const withMDX = require('@next/mdx')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    removeConsole: true,
+  },
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 };
 
